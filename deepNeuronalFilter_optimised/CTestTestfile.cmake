9bf7f9b57905ca4fd3b6f9c37b69221d5bd90302
adam <adamenglebright@rocketmail.com>
2024-05-04 14:45:18 +0100

    Remove CMake files ignored by previous commit.

--- deepNeuronalFilter_optimised/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/magin/DST5_project/AIHearingAid/deepNeuronalFilter_optimised
-# Build directory: /home/magin/DST5_project/AIHearingAid/deepNeuronalFilter_optimised
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("audio_filter")
-subdirs("cvui")
